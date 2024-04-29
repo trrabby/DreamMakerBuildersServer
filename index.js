1fdece8606621a5cca54dd4a514efa349a77db3a
trrabby1@gmail.com <110276063+trrabby@users.noreply.github.com>
2024-04-30 00:39:36 +0600

    added email inquery

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ require('dotenv').config();
 const port = process.env.PORT || 4000; 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors({
-  origin: ["http://localhost:5173/", "https://fiberfution.firebaseapp.com/"]
-}));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -35,8 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
+    
     const database = client.db("art&craftDB");
     const itemCollection = database.collection("items");
 
@@ -61,12 +58,11 @@ async function run() {
       res.send(result);
     })
 
-    // app.get('/items/:email', async(req, res)=>{
-    //   const email= req.params.email;
-    //   const query = {email: email}
-    //   const result= await itemCollection.find(query).toArray();
-    //   res.send(result);
-    // })
+    app.get('/items-data/:email', async(req, res)=>{
+      console.log(req.params.email)
+    const result= await itemCollection.find({email: req.params.email}).toArray();
+      res.send(result);
+    })
 
     app.delete('/items/:id', async (req, res)=>{
       const id= req.params.id;
