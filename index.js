a4d2910fd60461ed6c5841ff916bf68f52b6e31c
trrabby1@gmail.com <110276063+trrabby@users.noreply.github.com>
2024-04-30 23:27:13 +0600

    added put

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const cors = require('cors');
-const app= express();
+const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 4000; 
+const port = process.env.PORT || 4000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Server is running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-  })
+  res.send('Server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
 
 
 
@@ -33,11 +33,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    
+
     const database = client.db("art&craftDB");
     const itemCollection = database.collection("items");
+    const itemCollection2 = database.collection("artCategory")
 
-    app.post('/items', async (req, res)=>{
+    app.post('/items', async (req, res) => {
       const item = req.body;
       // console.log(user, "from server")
       const result = await itemCollection.insertOne(item);
@@ -45,52 +46,111 @@ async function run() {
 
     });
 
-    app.get('/items', async(req, res)=>{
+    app.post('/artCategory', async (req, res) => {
+      const item = req.body;
+      // console.log(user, "from server")
+      // const result = await itemCollection.insertOne(item);
+      const result = await itemCollection2.insertOne(item);
+      res.send(result);
+
+    });
+
+    app.get('/items', async (req, res) => {
       const cursor = itemCollection.find()
-      const result= await cursor.toArray();
+      const result = await cursor.toArray();
       res.send(result)
     })
 
-    app.get('/items/:id', async(req, res)=>{
-      const id= req.params.id;
-      const query={_id: new ObjectId(id)};
-      const result= await itemCollection.findOne(query);
+    app.get('/artCategory', async (req, res) => {
+      const cursor = itemCollection2.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/artCategory-data/:subCategory', async (req, res) => {
+      console.log(req.params.subCategory)
+      const result = await itemCollection2.find({ sub_category: req.params.subCategory }).toArray();
+      res.send(result)
+    })
+
+    
+    app.get('/artCategory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemCollection2.findOne(query);
       res.send(result);
     })
 
-    app.get('/items-data/:email', async(req, res)=>{
+    app.get('/items/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.get('/items-data/:email', async (req, res) => {
       console.log(req.params.email)
-    const result= await itemCollection.find({email: req.params.email}).toArray();
+      const result = await itemCollection.find({ email: req.params.email }).toArray();
       res.send(result);
     })
 
-    app.delete('/items/:id', async (req, res)=>{
-      const id= req.params.id;
-      const query= {_id: new ObjectId(id)};
+    app.delete('/items/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const result = await itemCollection.deleteOne(query);
       res.send(result);
     })
 
-    app.put('/items/:id', async (req, res)=>{
-      const id= req.params.id;
-      const updateData =req.body;
-      const filter= {_id: new ObjectId(id)};
+    app.delete('/artCategory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemCollection2.deleteOne(query);
+      res.send(result);
+    })
+
+    app.put('/items/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
         $set: {
-            Img_url: updateData.Img_url, 
-            item_name: updateData.item_name, 
-            sub_category: updateData.sub_category, 
-            processing_time: updateData.processing_time, 
-            stock_status: updateData.stock_status, 
-            price: updateData.price, 
-            rating: updateData.rating, 
-            customization: updateData.customization, 
-            short_description: updateData.short_description, 
-            
+          Img_url: updateData.Img_url,
+          item_name: updateData.item_name,
+          sub_category: updateData.sub_category,
+          processing_time: updateData.processing_time,
+          stock_status: updateData.stock_status,
+          price: updateData.price,
+          rating: updateData.rating,
+          customization: updateData.customization,
+          short_description: updateData.short_description,
+
         },
       };
       const result = await itemCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+    
+    app.put('/artCategory/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          Img_url: updateData.Img_url,
+          item_name: updateData.item_name,
+          sub_category: updateData.sub_category,
+          processing_time: updateData.processing_time,
+          stock_status: updateData.stock_status,
+          price: updateData.price,
+          rating: updateData.rating,
+          customization: updateData.customization,
+          short_description: updateData.short_description,
+
+        },
+      };
+      const result = await itemCollection2.updateOne(filter, updateDoc, options);
       res.send(result);
     })
 
